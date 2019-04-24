c9c96c9874093126f7e4609dd0b357d7f5beff0e
lcnihao <318082789@qq.com>
2019-04-24 21:44:33 +0800

    update

--- 9.Special_Pythagorean_triplet.c
+++ 9.Special_Pythagorean_triplet.c
@@ -9,18 +9,17 @@
 
 int main() {
     int a, b, c, ret = 0;
-    for(a = 1; a < 333; a++) {
-        for(b = a + 1; b < 333; b++) {
-            c = 1000 - a - c;
+    for(a = 1; a < 1000; a++) {
+        for(b = a + 1; b < 1000; b++) {
+            c = 1000 - a - b;
             if(c <= b) break;
             if((a * a + b * b) == c * c) {
                 ret = 1;
                 break;
             }
-            if(ret) break;
         }
         if(ret) break;
     }
-    printf("%d %d %d %d",a, b, c, a*b*c);
+    printf("%d %d %d %d\n",a, b, c, a*b*c);
     return 0;
 }
