218ecb5a1cdc880c3630454cac00b2a2c8ac86d0
lcnihao <318082789@qq.com>
2019-06-15 18:55:31 +0800

    update

---
+++ 11.cpp
@@ -0,0 +1,42 @@
+/*************************************************************************
+	> File Name: 11.cpp
+	> Author: lichun
+	> Mail: 318082789@qq.com
+	> Created Time: 2019年06月15日 星期六 18时40分02秒
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+#define MAX_N 20
+
+int dir[4][2] = {1, 0, 0, 1, 1, -1, 1, 1};
+
+int grid[MAX_N + 5][MAX_N + 5];
+
+int calc(int x, int y) {
+    int ans = 0;
+    for(int k = 0; k < 4; k++) {
+        int p = 1;
+        for (int step = 0; step < 4; step++) {
+            int dx = x + step * dir[k][0];
+            int dy = y + step * dir[k][1];
+            if (dx < 0 || dx >= MAX_N) break;
+            if (dy < 0 || dy >= MAX_N) break;
+            p *= grid[dx][dy];
+        }
+        if(p > ans) ans = p;
+    }
+    return ans;
+}
+
+int main() {
+    for (int i = 0; i < MAX_N; i++) {
+        for (int j = 0; j < MAX_N; j++) {
+            cin >> grid[i][j];
+        }
+    }
+    for(int i = 0; i < MAX_N; i++) {
+
+    }
+    return 0;
+}
--- tmp.cpp
+++ tmp.cpp
@@ -8,3 +8,16 @@
 #include <iostream>
 using namespace std;
 
+int arr[2000005] = {0};
+int main() {
+    long long  s = 0;
+    long long  n = 2000000;
+    for (int i = 2; i <= n; i++) {
+        if (!arr[i]) s += i;
+        for (int j = 2; i * j <= n; j++) {
+            arr[i * j] = 1;
+        }
+    }
+    cout << s << endl;
+    return 0;
+}
